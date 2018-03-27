54619d5ab85bc69790d6ee91925518a8a28ddbe3
jslby <me@jslby.ru>
2018-03-27 22:14:20 +0300

    добавлен блок 5

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
+require('slick-carousel');
 
 jQuery(function() {
-  
+  $('.slick1').slick();
 });
