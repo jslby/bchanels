5858a89caccf790a8d941f1ac81e26a0d774bbc8
jslby <me@jslby.ru>
2018-03-28 22:34:43 +0300

    все блоки

--- src/js/app.js
+++ src/js/app.js
@@ -5,4 +5,11 @@ import slick from 'slick-carousel';
 
 jQuery(function($) {
   $('.slick').slick();
+  $('a.toSlide').on('click', function(){
+    var id = $(this).data('id');
+    $(this).parents('section').find('.slick').slick('slickGoTo', id);
+    $(this).parents('section').find('a.toSlide').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  });
 });
